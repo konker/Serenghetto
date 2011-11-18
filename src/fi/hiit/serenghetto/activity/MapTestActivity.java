c6d59074bd1d95a480e30f95c74265809d92a0cd
konker <konker@gmail.com>
2011-11-18 17:55:02 +0200

    Added some maptest stuff

---
+++ src/fi/hiit/serenghetto/activity/MapTestActivity.java
@@ -0,0 +1,30 @@
+package fi.hiit.serenghetto.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.google.android.maps.GeoPoint;
+import com.google.android.maps.MapView;
+import com.google.android.maps.MapController;
+import com.google.android.maps.MapActivity;
+
+import fi.hiit.serenghetto.R;
+
+
+public class MapTestActivity extends MapActivity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.maptest);
+        MapView mapView = (MapView) findViewById(R.id.mapview);
+        mapView.setBuiltInZoomControls(true);
+    }
+
+    @Override
+    protected boolean isRouteDisplayed() {
+        return false;
+    }
+}
+
